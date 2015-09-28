362cba7e4f3335e31c0bf32050aff12c8e76a4b3
Jonas Holbech <holbech@ohlordy.local>
2015-09-28 17:01:48 +0200

    project setup

--- js/Controls.js
+++ js/Controls.js
@@ -1,6 +1,37 @@
 var Controls ={
 
+    _enabled:false,
+    startTouchY:null,
+    startTouchX:null,
+    endTouchX:null,
+    endTouchY:null,
     initialize:function(){
-
+        document.addEventListener('touchstart', Controls.tStart, true);
+        document.addEventListener('touchmove', Controls.tMove, true);
+        document.addEventListener('touchend', Controls.tEnd, true);
+    },
+    enable:function(){
+        Controls._enabled=true;
+    },
+    disable:function(){
+        Controls._enabled=false;
+    },
+    tStart:function(e){
+        var touch = e.touches[0];
+        Controls.startTouchX=touch.pageX;
+        Controls.startTouchY=touch.pageY;
+    },
+    tMove:function(e){
+        var touch = e.touches[0];
+        Controls.endTouchX=touch.pageX;
+        Controls.endTouchY=touch.pageY;
+        Main.move(Controls.endTouchX, Controls.endTouchY);
+    },
+    tEnd:function(e){
+        var dx = Controls.endTouchX - Controls.startTouchX,
+            dy = Controls.endTouchY - Controls.startTouchY;
+        if(Math.abs(dx) > Math.abs(dy)){
+            //horizontal
+        }
     }
 }
--- js/Main.js
+++ js/Main.js
@@ -2,6 +2,7 @@
 var Main={
     stage:null,
     loader:null,
+    debugText:null,
     init:function(){
         console.log("Main initialized");
         this.stage = new createjs.Stage("canvas");
@@ -16,12 +17,23 @@ var Main={
 
         createjs.Ticker.setFPS(30);
         createjs.Ticker.on('tick', Tick.tock);
+        this.debugText=new createjs.Text('TEXT', '20px Verdana', '#000');
         var x = new Letter("A");
+        x.on("click", Main.clicked, this);
         this.stage.addChild(x)
         x.x=100;
         x.y=100;
+    },
+    clicked:function(e){
+        var l = e.currentTarget;
+        Controls.enable();
 
+    },
+    move:function(x,y){
+        console.log(x,y)
+        this.debugText.text=x+ " "+y;
     }
+
 
 };
 Main.init();
