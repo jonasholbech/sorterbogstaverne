7ac8c29717862e3e4b3c1acb3bc6cd6249e82178
Jonas Holbech <holbech@ohlordy.local>
2015-09-28 14:59:46 +0200

    project setup

--- js/Tick.js
+++ js/Tick.js
@@ -1,12 +1,8 @@
 "use strict";
 var Tick = {
     tock:function(e){
-        //TODO pause
-        if(Game.inBattle){
-            Game.scenes[Game.currentScene].tick();
-            Player.tick();
-        }
-        Game.stage.update(e);
+
+        Main.stage.update(e);
 
     }
 }
